f9f78084e76a9ad7c636a31e94162512998b2ad8
nielguy <loganx7@hotmail.com>
2022-09-15 13:14:21 +0530

    simple project for java

---
+++ Project1.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class Project1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Friend! The current Date and Time is " + new Date().toString() + ".");
+    }
+}
